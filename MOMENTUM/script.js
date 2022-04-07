6b1164b7d44972ab7e04a51ee5515605befa98a0
gaeon <gaeon0413@gmail.com>
2022-04-07 15:30:44 +0900

    2022.04.07

--- MOMENTUM/script.js
+++ MOMENTUM/script.js
@@ -1,49 +1,11 @@
-const title = document.querySelector(".hello:first-child h1"); 
-
-let time = 0;
+const h1 = document.querySelector("div.hello:first-child h1"); 
 
 function handleTitleClick(){
-    console.log("title was clicked!");
-    if(time % 2 == 0)
-        title.style.color="skyblue";
+    if(h1.style.color === "skyblue")
+        h1.style.color="tomato";
     else
-        title.style.color="black";
-    time++;
-}
-
-function handleMouseEnter(){
-    title.innerText = "Mouse is here!";
-    title.style.color="red";    
-}
-
-function handleMouseLeave(){
-    title.innerText = "Mouse is gone!";
-    title.style.color="black";    
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-    title.style.color = "beige";
-}
-
-function handleWindowCopy(){
-    alert("Copier!");
-}
-
-function handleWindowOffline(){
-    alert("No!");
-}
-
-function handleWindowOnline(){
-    alert("Everything is Good!");
+        h1.style.color="skyblue";
 }
 
 //title.onclick = handleTitleClick;
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
