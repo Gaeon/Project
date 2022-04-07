60a0bb5fadc3d9df144b65db16858e32309cd934
gaeon <gaeon0413@gmail.com>
2022-04-07 15:31:54 +0900

    2022.04.07

--- MOMENTUM/script.js
+++ MOMENTUM/script.js
@@ -1,10 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1"); 
 
 function handleTitleClick(){
-    if(h1.style.color === "skyblue")
-        h1.style.color="tomato";
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "skyblue")
+        newColor="tomato";
     else
-        h1.style.color="skyblue";
+        newColor="skyblue";
+    h1.style.color = newColor;
 }
 
 //title.onclick = handleTitleClick;
